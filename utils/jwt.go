767a7bffbc7fa1549ce2f74c206c05c1afca1010
cpzz18 <robbincfa@gmail.com>
2025-08-10 01:40:25 +0700

    `Added bcrypt and JWT functionality to utils package`

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID uint) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	secret := os.Getenv("JWT_SECRET")
+	return token.SignedString([]byte(secret))
+}

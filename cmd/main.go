0e92b293876e3aca146b1b09a9b23c0ca3b85d44
cpzz18 <robbincfa@gmail.com>
2025-08-10 11:51:27 +0700

    `Refactor database connection and authentication logic`

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,20 @@
-package cmd
+package main
 
 import (
 	"github.com/gin-gonic/gin"
 
 	"note-kay/config"
+	"note-kay/routes"
 )
 
 func main() {
 	r := gin.Default()
+
 	config.LoadEnv()
+
 	config.ConnectDB()
+
+	routes.SetupRoutes(r)
 
 	r.Run(":8080")
 }

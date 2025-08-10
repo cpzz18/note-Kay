16ca56a4a78059aee5e331f6356338507235d243
cpzz18 <robbincfa@gmail.com>
2025-08-10 15:47:43 +0700

    `feat: Added note controller and models, updated routes for notes, folders, and tags`

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -20,6 +20,7 @@ type LoginInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
+
 func Register(c *gin.Context) {
     var input RegisterInput
     if err := c.ShouldBindJSON(&input); err != nil {
@@ -39,7 +40,12 @@ func Register(c *gin.Context) {
         return
     }
 
-    token, _ := utils.GenerateJWT(user.ID)
+    token, err := utils.GenerateJWT(user.ID)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+        return
+    }
+
     c.JSON(http.StatusOK, gin.H{
         "message": "User registered successfully",
         "user": gin.H{
@@ -52,31 +58,30 @@ func Register(c *gin.Context) {
 
 
 func Login(c *gin.Context) {
-    var input LoginInput
-    if err := c.ShouldBindJSON(&input); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
+	var input LoginInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-    var user models.User
-    if err := config.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
-        c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-        return
-    }
+	var user models.User
+	if err := config.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
 
-    if !utils.CheckPasswordHash(input.Password, user.Password) {
-        c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-        return
-    }
+	if !utils.CheckPasswordHash(input.Password, user.Password) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
 
-    token, _ := utils.GenerateJWT(user.ID)
-    c.JSON(http.StatusOK, gin.H{
-        "message": "Login successful",
-        "user": gin.H{
-            "id":    user.ID,
-            "email": user.Email,
-        },
-        "token": token,
-    })
+	token, _ := utils.GenerateJWT(user.ID)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login successful",
+		"user": gin.H{
+			"id":    user.ID,
+			"email": user.Email,
+		},
+		"token": token,
+	})
 }
-
--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type User struct {
     gorm.Model
     Email    string `gorm:"uniqueIndex;not null"`
-    Password string `gorm:"not null"`
-    Notes    []Note
-    Folders  []Folder
+    Password string `gorm:"not null" json:"-"`
+    Notes    []Note `json:"-"`
+    Folders  []Folder `json:"-"`
 }
